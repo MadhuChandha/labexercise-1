a39596bfe2accbdb3190a9c52262955f4f2752cb
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-08-04 07:01:12 -0700

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,36 @@
+package labexercise1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author home
+ */
+public class prime {
+    public static void main (String[] args) 
+    {
+        int i, n, flag=0;
+        Scanner obj=new Scanner(System.in);
+        System.out.println("enter n");
+        n=obj.nextInt();
+        for(i=2; i<n; i++)
+        {
+            if(n%i==0)
+            {
+                flag=1;
+                break;
+            }
+            else
+                flag=0;
+            
+                
+        }    
+        
+        if(flag==0)
+            System.out.println("prime number");
+        else
+            System.out.println("Not a prime number");
+        
+        
+    }
+    
+}

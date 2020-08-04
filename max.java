f9829db815fabdfc271c9922ba58a838e7033612
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-08-04 06:59:56 -0700

    Add files via upload

---
+++ max.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labexercise1;
+
+/**
+ *
+ * @author home
+ */
+public class max {
+    public static void main(String[] args)
+    {
+      
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);//run through Fn+F6 and click the pgm required....
+        
+        
+        if(a>b)
+            System.out.println("a is greater than b");
+        else
+            System.out.println("b is greater");
+    }
+    
+}

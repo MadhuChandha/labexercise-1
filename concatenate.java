0299df1b225c7bca0a33030b3be75182e8acf2ed
MadhuChandha <68582951+MadhuChandha@users.noreply.github.com>
2020-08-04 06:57:33 -0700

    Add files via upload

---
+++ concatenate.java
@@ -0,0 +1,25 @@
+package labexercise1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author home
+ */
+public class concatenate {
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner obj=new Scanner (System.in);
+        System.out.println("Enter the value of n");
+        n=obj.nextInt();
+        String result="";
+        
+        for(int i=1;i<=n;i++)
+        {
+            result=result+i+"";
+        }
+        System.out.println(result);
+    }
+    
+}
